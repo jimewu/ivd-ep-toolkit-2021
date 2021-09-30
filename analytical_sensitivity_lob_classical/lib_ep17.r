ef568a97254ea3d298a98d736362fa5cfebb6f4a
Jun-I Wu <jimewu@gmail.com>
2021-09-30 18:19:09 +0800

    完成須分4種狀況計算lob

--- analytical_sensitivity_lob_classical/lib_ep17.r
+++ analytical_sensitivity_lob_classical/lib_ep17.r
@@ -15,45 +15,183 @@ lapply(
 )
 
 # * analysis
-if (
-    # 條件1: 資料為常態
-    shapiro.test(data_ep17lob_tidy_combine$y)$p.value < 0.05 &
-        # 條件2: reagent_lot ≥4
-        length(
-            levels(
-                factor(data_ep17lob_tidy_combine$reagent_lot)
-            )
-        ) >= 4
-) {
-    mean_blank <- mean(data_ep17lob_tidy_combine$y)
-    sd_blank <- sd(data_ep17lob_tidy_combine$y)
+## 定義常態資料計算lob算法
+get_lob_para <- function(data, sample.lot) {
+    mean_blank <- mean(data)
+    sd_blank <- sd(data)
 
-    c_blank <- qnorm(0.95) / (
-        1 - (
-            4 * (
-                length(data_ep17lob_tidy_combine$y) - length(
-                    levels(
-                        factor(data_ep17lob_tidy_combine$sample_lot)
-                    )
-                )
-            )
-        )^(-1)
-    )
+    num_test <- length(data)
 
+    # 先算c_blank分母(denominator)
+    denom <- 1 - (4 * num_test - 4 * sample.lot)^(-1)
 
+    c_blank <- qnorm(0.95) / denom
 
     lob <- mean_blank + c_blank * sd_blank
-} else {
 
+    result <- data.frame(
+        mean = mean_blank,
+        sd = sd_blank,
+        lob = lob
+    )
+
+    return(result)
 }
 
+## 定義非常態資料計算lob算法
+get_lob_nonpara <- function(data) {
+    mean_blank <- mean(data)
+    sd_blank <- sd(data)
+
+    data <- sort(data)
+
+    rank_position <- 0.5 + 0.95 * length(data)
+
+    upr <- ceiling(rank_position)
+    lwr <- floor(rank_position)
+
+    lob <- ifelse(
+        upr == lwr,
+        data[rank_position],
+        mean(
+            data[lwr],
+            data[upr]
+        )
+    )
+
+    result <- data.frame(
+        mean = mean_blank,
+        sd = sd_blank,
+        lob = lob
+    )
+
+    return(result)
+}
+
+# 分析中間值
+imd <- vector(mode = "list")
+
+# 資料是否為常態
+imd[["spw-p"]] <- shapiro.test(data_ep17lob_tidy_combine$y)$p.value
+
+# reagent_lot數量
+imd[["reagent_lot"]] <- data_ep17lob_tidy_combine$reagent_lot %>%
+    factor() %>%
+    levels() %>%
+    length()
+
+# sample_lot數量
+imd[["sample_lot"]] <- data_ep17lob_tidy_combine$sample_lot %>%
+    factor() %>%
+    levels() %>%
+    length()
 
 
+# 區分情況計算lob
+if (
+    # 條件1:資料為常態
+    imd[["spw-p"]] >= 0.05 &
+        ## 條件2: reagent_lot ≥4
+        imd[["reagent_lot"]] >= 4
+) {
+    # 則不分批計算常態lob
+    data_ep17lob_analysis_lob <- get_lob_para(
+        data_ep17lob_tidy_combine$y,
+        imd[["sample_lot"]]
+    )
 
-## Shaprio-Wilk Analysis
-data_ep17lob_analysis_spw <- shapiro.test(
-    data_ep17lob_tidy_combine$y
-)
+    # 紀錄狀況判斷結果
+    imd[["crit"]] <- data.frame(
+        normality = TRUE,
+        by_lot = FALSE
+    )
+} else if (
+    # 條件1:資料為非常態
+    imd[["spw-p"]] < 0.05 &
+        ## 條件2: reagent_lot ≥4
+        imd[["reagent_lot"]] >= 4
+) {
+    # 則不分批計算非常態lob
+    data_ep17lob_analysis_lob <- get_lob_nonpara(
+        data_ep17lob_tidy_combine$y
+    )
+
+    # 紀錄狀況判斷結果
+    imd[["crit"]] <- data.frame(
+        normality = FALSE,
+        by_lot = FALSE
+    )
+} else if (
+    # 條件1:資料為常態
+    imd[["spw-p"]] >= 0.05 &
+        ## 條件2: reagent_lot ≤3
+        imd[["reagent_lot"]] <= 3
+) {
+    # 則分批計算常態lob
+    imd[["lot_lob"]] <- lapply(
+        data_ep17lob_tidy_split,
+        function(x) {
+            sample_lot <- x$sample_lot %>%
+                factor() %>%
+                levels() %>%
+                length()
+
+            result <- get_lob_para(
+                x$y,
+                sample_lot
+            )
+        }
+    )
+
+    # 紀錄狀況判斷結果
+    imd[["crit"]] <- data.frame(
+        normality = TRUE,
+        by_lot = TRUE
+    )
+} else if (
+    # 條件1:資料為非常態
+    imd[["spw-p"]] < 0.05 &
+        ## 條件2: reagent_lot ≤3
+        imd[["reagent_lot"]] <= 3
+) {
+    # 則分批計算非常態lob
+    imd[["lot_lob"]] <- lapply(
+        data_ep17lob_tidy_split,
+        function(x) {
+            result <- get_lob_nonpara(x$y)
+        }
+    )
+
+    # 紀錄狀況判斷結果
+    imd[["crit"]] <- data.frame(
+        normality = FALSE,
+        by_lot = TRUE
+    )
+}
+
+if (
+    # 如果有分批lob
+    is.null(imd[["lot_lob"]]) == FALSE
+) {
+    # 把各批合併
+    data_ep17lob_analysis_lob <- imd[["lot_lob"]][[1]]
+    for (
+        x in seq(
+            2, length(imd[["lot_lob"]])
+        )
+    ) {
+        data_ep17lob_analysis_lob <- rbind(
+            data_ep17lob_analysis_lob,
+            imd[["lot_lob"]][[x]]
+        )
+    }
+
+    # 加上final lob
+    data_ep17lob_analysis_lob <- data_ep17lob_analysis_lob %>%
+        mutate(
+            final_lob = max(lob)
+        )
+}
 
 # * report_fig
 data_ep5_report_fig_raw <- ggplot(
