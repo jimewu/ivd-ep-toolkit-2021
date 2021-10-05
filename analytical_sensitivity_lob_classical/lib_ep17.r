3deb9ea7e6c37ae0fb7eb96278cffe6a95ac42fe
Jun-I Wu <jimewu@gmail.com>
2021-10-05 17:56:32 +0800

    刪除多餘檔案，整理ep17 LoB變數表

--- analytical_sensitivity_lob_classical/lib_ep17.r
+++ analytical_sensitivity_lob_classical/lib_ep17.r
@@ -32,6 +32,7 @@ get_lob_para <- function(data, sample.lot) {
     result <- data.frame(
         mean = mean_blank,
         sd = sd_blank,
+        cp = c_blank,
         lob = lob
     )
 
@@ -62,6 +63,8 @@ get_lob_nonpara <- function(data) {
     result <- data.frame(
         mean = mean_blank,
         sd = sd_blank,
+        lwr = data[lwr],
+        upr = data[upr],
         lob = lob
     )
 
@@ -194,148 +197,69 @@ if (
 }
 
 # * report_fig
-data_ep5_report_fig_raw <- ggplot(
-    data_ep5_tidy_combine,
-    aes(
-        x = factor1,
-        y = y,
-        color = factor(factor2)
-    )
+data_ep17lob_report_fig_raw <- ggplot(
+    data_ep17lob_tidy_combine,
+    aes(y)
 ) +
-    geom_point() +
-    facet_wrap(
-        facets = ~sample,
-        scales = "free",
-        ncol = 1
-    ) +
-    xlab(setting_ep5$name_of_factors[1]) +
-    ylab(setting_ep5$unit_of_device[1]) +
-    labs(
-        color = setting_ep5$name_of_factors[2]
-    ) +
-    scale_x_continuous(
-        breaks = seq(1, 20, by = 1)
-    )
+    geom_histogram(bins = 30)
 
-data_ep5_report_fig_lj <- ggplot(
-    data_ep5_tidy_combine,
+
+data_ep17lob_report_fig_qq <- ggplot(
+    data_ep17lob_tidy_combine,
     aes(
-        x = factor1,
-        y = y_lj,
-        color = factor(factor2)
+        sample = y
     )
 ) +
-    geom_point() +
-    facet_wrap(
-        facets = ~sample,
-        scales = "free",
-        ncol = 1
+    stat_qq(
+        alpha = 0.75
     ) +
-    geom_hline(
-        yintercept = 0,
-        color = "black",
-        alpha = 0.3
-    ) +
-    geom_hline(
-        yintercept = c(
-            1:3,
-            (-1):(-3)
-        ),
-        color = "black",
-        alpha = 0.3,
+    geom_qq_line(
+        color = "red",
         linetype = 2
     ) +
-    xlab(setting_ep5$name_of_factors[1]) +
-    ylab("Levey-Jennings Scale") +
-    labs(
-        color = setting_ep5$name_of_factors[2]
-    ) +
-    scale_y_continuous(
-        breaks = seq(-3, 3, by = 1)
-    ) +
-    scale_x_continuous(
-        breaks = seq(1, 20, by = 1)
+    xlab("Normal Distribution (Levey-Jennings Scale)") +
+    ylab(
+        paste(
+            "Sample",
+            " (Shapiro-Wilk Normality Test p: ",
+            round(
+                shapiro.test(data_ep17lob_tidy_combine$y)$p.value,
+                digits = 3
+            ),
+            ")",
+            sep = ""
+        )
     )
 
 # * report_tab
-## 加入前置欄位以方便識別
-data_ep5_report_tab_split <- lapply(
-    seq(1, length(data_ep5_analysis)),
-    function(x) {
-        # 調整列的順序，total移到最下面
-        data <- data_ep5_analysis[[x]][
-            c(
-                seq(2, nrow(data_ep5_analysis[[x]])),
-                1
-            ),
-        ]
-
-        cbind(
-            Sample = names(data_ep5_analysis)[x],
-            Item = c(
-                setting_ep5$name_of_factors,
-                "Error",
-                "Total"
-            ),
-            round(
-                data,
-                digits = 3
-            )
-        )
-    }
-)
-
-# * data_report_tab
-if (
-    length(data_ep5_report_tab_split) == 1
-) {
-    data_ep5_report_tab <- data_ep5_report_tab_split[[1]]
-} else if (
-    length(data_ep5_report_tab_split) > 1
-) {
-    data_ep5_report_tab <- data_ep5_report_tab_split[[1]]
-
-    for (
-        x in seq(2, length(data_ep5_report_tab_split))
-    ) {
-        data_ep5_report_tab <- rbind(
-            data_ep5_report_tab,
-            data_ep5_report_tab_split[[x]]
-        )
-    }
-}
-
-## 使用datatable產生動態表格
-data_ep5_report_tab <- data_ep5_report_tab %>%
-    datatable(
-        rownames = FALSE
+data_ep17lob_report_tab <- data_ep17lob_analysis_lob %>%
+    round(digits = 3) %>%
+    flextable() %>%
+    merge_v() %>%
+    set_header_labels(
+        mean = "Mean",
+        sd = "SD",
+        lwr = "Rank Position: Lwr.",
+        upr = "Rank Position: Upr.",
+        lob = "LoB",
+        final_lob = "Final LoB"
+    ) %>%
+    align(
+        part = "header",
+        align = "center"
+    ) %>%
+    align(
+        part = "body",
+        align = "center"
     )
 
-
-# data_ep5_report_tab <- data_ep5_report_tab %>%
-#     flextable() %>%
-#     merge_v(j = "Sample") %>%
-#     align(
-#         align = "center",
-#         part = "all"
-#     ) %>%
-#     footnote(
-#         part = "header",
-#         j = c(
-#             "DF",
-#             "SS",
-#             "MS",
-#             "VC"
-#         ),
-#         value = as_paragraph(
-#             c(
-#                 "Degrees of freedom",
-#                 "sum of squares",
-#                 "mean squares",
-#                 "variance components"
-#             )
-#         ),
-#         ref_symbols = c("a", "b", "c", "d"),
-#         inline = TRUE,
-#         sep = "; "
-#     )
+if (
+    is.null(data_ep17lob_analysis_lob$cp) == FALSE
+) {
+    data_ep17lob_report_tab <- data_ep17lob_report_tab %>%
+        compose(
+            part = "header",
+            j = "cp",
+            value = as_paragraph("C", as_sub("P")),
+        )
+}
